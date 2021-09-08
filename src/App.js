cc898c14f5eb149153dac65ff664bb9952722f2f
Mir Hussain <mir.hm110@gmail.com>
2021-09-08 13:04:39 +0600

    working in example two

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ExampleOne from "./ExampleOne";
+import ExampleTwo from "./ExampleTwo";
 const App = () => {
   return (
     <>
       <ExampleOne />
+      <ExampleTwo />
     </>
   );
 };
